bd84c281d3a765ac77ecd5116ceec316b55aba2f
Chinmayee Ahire <theroadtotae@gmail.com>
2023-01-29 17:20:00 +0530

    Added tweet services with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,60 @@
 const express=require('express');
 const connect=require('./config/database');
-const Tweet=require('./models/tweet');
+// const {TweetRepository}=require('./repository/index');
+// const TweetService=require('./services/tweet-service');
+// const Tweet=require('./models/tweet');
 // const TweetRepository=require('./repository/tweet-repository');
 // const Comment=require('./models/comment');
 
 const app=express();
+
 
 app.listen(3000, async ()=>{
 console.log("Server started");
 await connect();
 console.log("Mongo DB connected");
 
-const tweets =await Tweet.find({
-    content:
-        ["second tweet", "First tweet", "tweet updated twice 3"]
+// const tweets =await Tweet.find({
+//     content:
+//         ["second tweet", "First tweet", "tweet updated twice 3"]
 
-    });
-console.log(tweets);
+//     });
+// console.log(tweets);
 
+//  let repo=new HashtagRepository();
+//   await repo.bulkCreate([
+//     {
+//         title: 'Trend',
+//         tweets: []
+//     },
+//     {
+//         title: 'Excited',
+//         tweets: []
+//     },
+//     {
+//         title: 'Python',
+//         tweets: []
+//     },
+//     {
+//         title: 'Fun',
+//         tweets: []
+//     },
+//     {
+//         title: 'Career',
+//         tweets: []
+//     }
+//   ]);
+
+// let response=await repo.findByName(['Excited', 'Trend']);
+// console.log(response);
+// response=response.map(tags=>tags.title);
+// console.log(response);
+
+
+// let service=new TweetService();
+// const tweet=await service.create({
+//     content: 'My #working twitter!!'
+// });
+// console.log(tweet);
 });
 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,22 +1,39 @@
-const {TweetRepository}=require('../repository/index');
+const {TweetRepository, HashtagRepository}=require('../repository/index');
 
 
 class TweetService {
   constructor(){
     this.tweetRepository=new TweetRepository();
+    this.hashtagRepositry= new HashtagRepository();
   }
 
     async create(data){
     const content=data.content;
-    const tags=content.match(/#[a-zA-Z0-9_]+/g);  // this regular expression extracts hashtags
-    tags=tags.map((tag)=> tag.substring(1));
-    console.log(tags);
+    const tags=await content.match(/#[a-zA-Z0-9_]+/g).map((tag)=> tag.substring(1));  // this regular expression extracts hashtags
+  
     const tweet=await this.tweetRepository.create(data);
+    let alreadyPresentTags=await this.hashtagRepositry.findByName(tags);
+  
+    let titleofPresenttags=alreadyPresentTags.map(tags=>tags.title) 
+    
+    let newTags=tags.filter(tag=>!titleofPresenttags.includes(tag));
+    newTags=newTags.map(tag=>{
+      return {title: tag, tweets: [tweet.id]}
+    });
+    await this.hashtagRepositry.bulkCreate(newTags);
+    alreadyPresentTags.forEach((tag) => {
+      tag.tweets.push(tweet.id);
+      tag.save();
+    });
+    
+    //[{title: 'coding'},  tweets:[]]
+    //[excited,coding,js]-> [{title: excited}, {title: career}]
+    //[excited,coding,js]-> [ excited, career]
     //todo create hashtags and add here
     /*
      1. bulkcreate in mongoose
      2. filter title of hashtag based on multiple tags
-     3.how to add tweetid inside all the hashtags
+     3. how to add tweetid inside all the hashtags
     */
     return tweet;
    }
