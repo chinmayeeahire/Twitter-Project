e77a5a7c81beaaabfd6b1f4a4e2823d284e833ea
Chinmayee Ahire <theroadtotae@gmail.com>
2023-02-01 13:28:33 +0530

    Added Basic crud repo and extended it in like repo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -2,34 +2,39 @@
 // const Tweet=require('../models/tweet');
 
 import Tweet from '../models/tweet.js';
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
+class TweetRepository extends CrudRepository {
+
+    constructor(){
+        super(Tweet);
+    }
     
     async create(data){
-        try {
+         try {
             const tweet=await Tweet.create(data);
             return tweet;
          } catch (error) {
              console.log(error);
          }        
     }
-    async get(id){
-        try {
-            const tweet=await Tweet.findById(id);
-            return tweet;
-         } catch (error) {
-             console.log(error);
-         }
-    }
+    // async get(id){
+    //     try {
+    //         const tweet=await Tweet.findById(id);
+    //         return tweet;
+    //      } catch (error) {
+    //          console.log(error);
+    //      }
+    // }
    
-    async destroy(id){
-        try {
-            const tweet=await Tweet.findByIdAndRemove(id);
-            return tweet;
-         } catch (error) {
-             console.log(error);
-         }
-    }
+    // async destroy(id){
+    //     try {
+    //         const tweet=await Tweet.findByIdAndRemove(id);
+    //         return tweet;
+    //      } catch (error) {
+    //          console.log(error);
+    //      }
+    // }
 
 
     async getAll(offset, limit){
