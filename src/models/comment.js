7a928576d54d89ad860c11baf476737e2b1f156d
Chinmayee Ahire <theroadtotae@gmail.com>
2023-02-04 16:58:50 +0530

    Updated comment model with ref path

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,9 +6,24 @@ const commentSchema=new mongoose.Schema({
         required: true,
 
     },
-    userEmail:{
-        type: String
+    // userEmail:{
+    //     type: String
+    // },
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
+    onModel:{
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
     
 }, {timestamps: true});
 
